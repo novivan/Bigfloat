d61813fae73251e845328058bbda479f5e47e88c
nov_ivan (Иван) <nov_ivan_777@mail.ru>
2024-02-20 16:20:32 +0300

    added showing amount of time spent on counting pi

--- pi.cpp
+++ pi.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <chrono>
 
 #include "BigFloat.h"
 
@@ -8,9 +8,47 @@ int main() {
 
     std::cout << "Hello, BigFloat!" << std::endl;
 
-    /*
+    std::cout << "Enter a precision (amount of digits after '.') for pi" << std::endl;
+    int n;
+    std::cin >> n;
 
-    int n; std::cin >> n;
+
+    auto begin_time = std::chrono::high_resolution_clock::now();
+
+    BigFloat pi = BigFloat(0);
+    pi.set_precision(n);
+
+    BigFloat bf_1 = BigFloat(1);
+    BigFloat bf_2 = bf_1 + bf_1;
+    BigFloat bf_4 = bf_2 * bf_2;
+    BigFloat bf_8 = bf_4 * bf_2;
+    BigFloat bf_6 = bf_8 - bf_2;
+    BigFloat bf_5 = bf_6 - bf_1;
+
+    BigFloat common_denom = bf_1;
+    BigFloat base = bf_4 * bf_4;
+
+    for (int k = 0; k <= n; k++) {
+        pi += bf_1 / common_denom * (
+                (bf_4 / (bf_8 * BigFloat(k) + bf_1))
+                - (bf_2 / (bf_8 * BigFloat(k) + bf_4))
+                - (bf_1 / (bf_8 * BigFloat(k) + bf_5))
+                - (bf_1 / (bf_8 * BigFloat(k) + bf_6))
+        );
+
+        common_denom *= base;
+    }
+
+
+    pi.write();
+    std::cout << "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196" <<'\n';
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto amount_of_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - begin_time);
+    std::cout << amount_of_time.count() << "ms" << std::endl;
+
+    /*
+    int amount; std::cin >> amount;
     for (int i = 0; i < n; i++) {
         BigFloat a, b;
         a.read();
@@ -43,45 +81,8 @@ int main() {
         std::cout << "b / a = ";
         div_b_a.write();
     }
+
     */
-
-
-    std::cout << "Enter a precision (amount of digits after '.') for pi" << std::endl;
-    int n;
-    std::cin >> n;
-
-    BigFloat pi = BigFloat(0);
-
-    pi.set_precision(n + 5);
-
-    BigFloat bf_1 = BigFloat(1);
-    BigFloat bf_2 = bf_1 + bf_1;
-    BigFloat bf_4 = bf_2 * bf_2;
-    BigFloat bf_8 = bf_4 * bf_2;
-    BigFloat bf_6 = bf_8 - bf_2;
-    BigFloat bf_5 = bf_6 - bf_1;
-
-    BigFloat common_denom = bf_1;
-    BigFloat base = bf_4 * bf_4;
-
-    for (int k = 0; k <= n + 2; k++) {
-        pi += bf_1 / common_denom * (
-                (bf_4 / (bf_8 * BigFloat(k) + bf_1))
-                - (bf_2 / (bf_8 * BigFloat(k) + bf_4))
-                - (bf_1 / (bf_8 * BigFloat(k) + bf_5))
-                - (bf_1 / (bf_8 * BigFloat(k) + bf_6))
-                );
-
-        common_denom *= base;
-    }
-
-    pi.set_precision(n);
-
-    pi.write();
-    std::cout << "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811" <<'\n';
-
-
-
 
 
     return 0;
